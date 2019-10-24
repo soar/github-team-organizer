50a25b0f8ffda5534c9215a90f836f445b5718a6
Aleksey @soar Smyrnov <i@soar.name>
2019-10-24 20:07:59 +0300

    Add setup script

---
+++ setup.py
@@ -0,0 +1,41 @@
+import os
+from distutils.core import setup
+from setuptools import find_packages
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
+
+setup(
+    name='github-team-organizer',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://soar.name',
+    license='MIT',
+    author='soar',
+    author_email='i@soar.name',
+    description='Tool for organizing your GitHub Organization',
+    long_description=read('README.rst'),
+    entry_points={
+        'console_scripts': [
+            'team-organizer = github_team_organizer.scripts.organizer:run'
+        ]
+    },
+    install_requires=[
+        'dotenv',
+        'pygithub',
+        'python-dotenv',
+        'click',
+        'cached-property',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: MIT',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+)
